6a172137a64e94ce9ff1cfca9c2351a39fe9cf7c
WeiZhiHui10 <1023016663@qq.com>
2020-01-13 17:04:16 +0800

    商品规格参数分页显示

--- ego-manager/src/main/java/com/ego/manager/controller/TbItemParamController.java
+++ ego-manager/src/main/java/com/ego/manager/controller/TbItemParamController.java
@@ -1,4 +1,27 @@
 package com.ego.manager.controller;
 
+import com.ego.commons.pojo.EasyUIDataGrid;
+import com.ego.manager.service.TbItemParamService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
 public class TbItemParamController {
+    @Resource
+    private TbItemParamService tbItemParamServiceImpl;
+
+    /**
+     * 规格参数的分页显示
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping("item/param/list")
+    @ResponseBody
+    public EasyUIDataGrid showPage(int page,int rows){
+        return tbItemParamServiceImpl.showPage(page, rows);
+    }
 }
